52e46ad29c5a675bfc37a4c35a4531ec9de993bc
steve <gadafisteve001@gmail.com>
2025-09-17 15:03:02 +0300

    updates

--- kiharu_bursary_allocation_system/settings.py
+++ kiharu_bursary_allocation_system/settings.py
@@ -34,6 +34,7 @@ AUTH_USER_MODEL = 'kiharu_system.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,7 +116,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    # for `collectstatic` in 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+# WhiteNoise configuration
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
