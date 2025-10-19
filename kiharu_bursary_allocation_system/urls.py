7870a9cf85371f49aaa5a9b5e38b9da55d758edf
steve <gadafisteve001@gmail.com>
2025-10-19 17:22:44 +0300

    made configurations

--- kiharu_bursary_allocation_system/urls.py
+++ kiharu_bursary_allocation_system/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +24,14 @@ handler403 = 'kiharu_system.views.custom_permission_denied'
 handler404 = 'kiharu_system.views.custom_page_not_found'
 handler500 = 'kiharu_system.views.custom_server_error'
 
+
+# This allows media files to be served even when DEBUG = False (for dev/testing)
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
+
+
+    
